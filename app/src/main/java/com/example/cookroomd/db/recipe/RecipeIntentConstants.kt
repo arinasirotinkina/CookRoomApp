c56e968d7c33fab9ef84fd04e3219922e5f5e4c9
arina <92909580+arinasirotinkina@users.noreply.github.com>
2022-10-05 20:20:44 +0300

    Delete app directory

--- app/src/main/java/com/example/cookroomd/db/recipe/RecipeIntentConstants.kt
+++
@@ -1,7 +0,0 @@
-package com.example.cookroomd.db.recipe
-
-object RecipeIntentConstants {
-    const val I_TITLE_KEY = "title_key"
-    const val I_DESC_KEY = "description_key"
-    const val I_ID_KEY = "id_key"
-}
--- app/src/main/java/com/example/cookroomd/model/ProdItem.kt
+++
@@ -1,9 +0,0 @@
-package com.example.cookroomd.model
-
-class ProdItem {
-    var id : Int? = 0
-    var title : String? = null
-    var category :String? = null
-    var amount :Int? = null
-    var measure: String? = null
-}
