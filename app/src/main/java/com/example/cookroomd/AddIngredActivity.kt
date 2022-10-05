1d3451315078a14e2ffccce03fe06cff2d47cb5c
arina <arinasirotinkina2381@gmail.com>
2022-10-05 20:02:11 +0300

    'Added'

---
+++ app/src/main/java/com/example/cookroomd/AddIngredActivity.kt
@@ -0,0 +1,11 @@
+package com.example.cookroomd
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class AddIngredActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_add_ingred)
+    }
+}
---
+++ app/src/main/java/com/example/cookroomd/model/CategoryItem.kt
@@ -0,0 +1,11 @@
+package com.example.cookroomd.model
+
+class CategoryItem {
+    var icons: Int? = 0
+    var name: String? = null
+
+    constructor(icons: Int?, name: String?) {
+        this.icons = icons
+        this.name = name
+    }
+}
