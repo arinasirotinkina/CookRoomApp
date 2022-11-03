082adf6ab9934022a87ece318dcad24c423057c4
arina <arinasirotinkina2381@gmail.com>
2022-11-03 16:06:36 +0300

    commit

--- app/src/main/java/com/example/cookroom/LoginActivity.kt
+++ app/src/main/java/com/example/cookroom/LoginActivity.kt
@@ -20,7 +20,7 @@ class LoginActivity : AppCompatActivity() {
     var email : EditText? = null
     var password : EditText? = null
     var loginButton : Button? = null
-    var reglink : TextView? = null
+    var notHaveAcc : TextView? = null
     var sessionManager = SessionManager(this)
     companion object  {
         var URL_LOGIN = "https://cookroom.site/login.php"
@@ -31,38 +31,40 @@ class LoginActivity : AppCompatActivity() {
         email = findViewById(R.id.email)
         password = findViewById(R.id.password)
         loginButton = findViewById(R.id.loginButton)
-        reglink = findViewById(R.id.reglink)
+        notHaveAcc = findViewById(R.id.notHaveAcc)
         sessionManager.sessionManage(this)
         loginButton?.setOnClickListener() {
-            var mEmail : String = email?.text.toString().trim()
-            var mPassword : String = password?.text.toString().trim()
+            val mEmail : String = email?.text.toString().trim()
+            val mPassword : String = password?.text.toString().trim()
 
             if (mEmail.isNotEmpty() || mPassword.isNotEmpty()) {
-                Login(mEmail, mPassword)
+                login(mEmail, mPassword)
             }
         }
-        reglink?.setOnClickListener() {
+        notHaveAcc?.setOnClickListener() {
             var i = Intent(this, RegistrationActivity::class.java)
             startActivity(i)
+            finish()
         }
 
     }
-    private fun Login(email: String, password: String){
+    private fun login(email: String, password: String){
         var stringRequest = object : StringRequest(
             Method.POST, LoginActivity.URL_LOGIN,
             Response.Listener<String> { response ->
                 try {
                     val jsonObject = JSONObject(response.toString())
                     val success = jsonObject.getString("success")
+                    val message = jsonObject.getString("message")
                     val jsonArray = jsonObject.getJSONArray("login")
+                    Toast.makeText(this, message.toString(), Toast.LENGTH_LONG).show()
                     if (success.equals("1")) {
                         for (i in 0 until jsonArray.length()) {
-                            var obj = jsonArray.getJSONObject(i)
-                            var email = obj.getString("email").trim()
-                            var id = obj.getString("id").trim()
+                            val obj = jsonArray.getJSONObject(i)
+                            val email = obj.getString("email").trim()
+                            val id = obj.getString("id").trim()
                             val time = obj.getString("time").trim()
                             sessionManager.createSession(this, email, id, time)
-                            //Toast.makeText(this, time, Toast.LENGTH_LONG).show()
                             val i = Intent(this, MainActivity::class.java)
                             i.putExtra("email", email)
                             startActivity(i)
@@ -82,14 +84,14 @@ class LoginActivity : AppCompatActivity() {
 
             @Throws(AuthFailureError::class)
             override fun getParams(): Map<String, String>? {
-                var params : HashMap<String, String> = HashMap<String, String>()
-                params.put("email", email)
-                params.put("password", password)
+                val params : HashMap<String, String> = HashMap<String, String>()
+                params["email"] = email
+                params["password"] = password
                 return params
 
             }
         }
-        var requestQueue = Volley.newRequestQueue(this)
+        val requestQueue = Volley.newRequestQueue(this)
         requestQueue.add(stringRequest)
 
     }
